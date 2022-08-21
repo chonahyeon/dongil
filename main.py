5815b0fa04d66a8d3bdc2f81f80b8b7884116444
Jeon-peng <pengping@kakao.com>
2022-08-21 20:01:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ with st.sidebar.header('0. Select CSV or Model'):
     else :
         with st.sidebar.header('예측 모델을 선택해주세요'):
 
-            model_list = list(filter(os.path.isdir, os.listdir('./model')))
+            model_list = list(filter(os.path.isdir, os.listdir('./model/')))
             uploaded_file = './입찰데이터_수정용_0804.csv'
 
             model_value = st.selectbox("모델 선택 ", model_list, format_func=lambda x: model_list[x])
