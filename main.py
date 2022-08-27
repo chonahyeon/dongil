bd9fdfd81982cd15697a80fc547630ff89e8d364
Jeon-peng <70497132+Jeon-peng@users.noreply.github.com>
2022-08-27 17:16:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -378,7 +378,7 @@ with st.sidebar.header('0. Select CSV or Model'):
             pred_ratio, pred_value = build_model(df)
             ### 이거이거이거이거이거
             global euclide_df
-            euclide_df = euclide_df.append({'공고번호': 9999, '낙찰하한율': float(ratio_list[ratio_value]), '연면적': float(land_area),'대지면적': float(build_area), '기초금액': float(cost), '예가율': float(pred_ratio)})
+            euclide_df = euclide_df.append({'공고번호': 9999, '낙찰하한율': float(ratio_list[ratio_value]), '연면적': float(land_area),'대지면적': float(build_area), '기초금액': float(cost), '예가율': float(pred_ratio)},ignore_index = True)
 
 
 
