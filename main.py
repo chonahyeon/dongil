3df88da30ec8fc7c7a7fc3798f7b92b6062ee938
Jeon-peng <70497132+Jeon-peng@users.noreply.github.com>
2022-08-27 19:18:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -381,14 +381,11 @@ with st.sidebar.header('0. Select CSV or Model'):
             ### 이거이거이거이거이거
             st.session_state['euclide_df'] = st.session_state['euclide_df'].append({'공고번호': 9999, '낙찰하한율': ratio_list[ratio_value], '연면적': land_area, '대지면적': build_area, '기초금액': cost,'예가율': float(pred_ratio)}, ignore_index=True)
             new_euclidean = st.session_state['euclide_df'].astype(float)
-            new_concat = st.session_state['concat_df'][['공고번호', '연면적', '대지면적', '기초금액', '낙찰하한율', '예가율']].astype(float)
+            new_concat = st.session_state['concat_df'][['공고번호', '연면적', '대지면적', '기초금액', '낙찰하한율', '예가율']]
             new_result = pd.concat([new_euclidean,new_concat])
             result_euclidean = pd.DataFrame(squareform(pdist(new_result.iloc[:, 1:])), columns=new_result['공고번호'].unique(),index=new_result['공고번호'].unique())
             list_e = list(result_euclidean.loc[9999].sort_values().head(10).index)
             st.session_state['result'] = st.session_state['concat_df'][st.session_state['concat_df']['공고번호'].isin(list_e)][['공고번호', '입찰날짜', '연면적', '대지면적', '기초금액', '낙찰하한율', '예가율']].reset_index(drop = True)
-            st.session_state['result']['연면적'] = pd.to_numeric(st.session_state['result']['연면적'])
-            st.session_state['result']['대지면적'] = pd.to_numeric(st.session_state['result']['대지면적'])
-            st.session_state['result']['기초금액'] = pd.to_numeric(st.session_state['result']['기초금액'])
 
                 # print('예측한 투찰율 : {:0,.4f}%'.format(float(pred_val) * 100))
                 # print('예측한 계산된 가격 : {0:,}'.format(int(pred_cost)))
