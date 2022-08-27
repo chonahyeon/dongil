ef82d9d598d473932af96394be18733d0576d1c8
Jeon-peng <70497132+Jeon-peng@users.noreply.github.com>
2022-08-27 18:09:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -379,8 +379,8 @@ with st.sidebar.header('0. Select CSV or Model'):
             pred_ratio, pred_value = build_model(df)
             ### 이거이거이거이거이거
             st.session_state['euclide_df'] = st.session_state['euclide_df'].append({'공고번호': 9999, '낙찰하한율': ratio_list[ratio_value], '연면적': land_area, '대지면적': build_area, '기초금액': cost,'예가율': float(pred_ratio)}, ignore_index=True)
-            concat_df = pd.read_csv('./euclidean.csv')[['공고번호','낙찰하한율', '연면적','대지면적','기초금액','예가율']]
 
+            st.session_state['concat_df'] =pd.concat(st.session_state['euclide_df'],st.session_state['concat_df'])
 
 
 
@@ -464,6 +464,10 @@ if st.button("타기업 분석"):
 st.subheader('3. 유사공고 분석 ')
 if 'euclide_df' not in st.session_state:
     st.session_state['euclide_df'] = pd.DataFrame(columns=['공고번호', '낙찰하한율', '연면적', '대지면적', '기초금액', '예가율'])
+
+if 'concat_df' not in st.session_state:
+    st.session_state['concat_df'] = pd.read_csv('./euclidean.csv')[['공고번호', '낙찰하한율', '연면적', '대지면적', '기초금액', '예가율']]
+
 if st.button('유사공고 확인하기'):
     st.write('유사공고 기업 분석')
     st.table(st.session_state['euclide_df'])
