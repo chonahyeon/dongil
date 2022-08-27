6235d23a7e835a8c1fbe30bd6f7bd968f18a2fdd
Jeon-peng <70497132+Jeon-peng@users.noreply.github.com>
2022-08-27 17:35:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -474,9 +474,9 @@ st.subheader('3. 유사공고 분석 ')
 
 st.write('유사공고 기업 분석')
 # euclide_df = pd.DataFrame(columns=['공고번호', '낙찰하한율', '연면적', '대지면적', '기초금액', '예가율'])
-
+euclide_df = pd.read_csv('./euclidean.csv').astype(float)[['공고번호', '낙찰하한율', '연면적', '대지면적', '기초금액', '예가율']]
 # if st.button("유사도분석"):
-st.table(euclide_df.tail(10))
+st.table(euclide_df)
 #     euclide_df = pd.DataFrame(columns=['공고번호', '낙찰하한율', '연면적', '대지면적', '기초금액', '예가율'])
 #
 
