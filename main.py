c0c844867cbe5d8a0e2bb7398a1dd4807fb68514
Jeon-peng <70497132+Jeon-peng@users.noreply.github.com>
2022-08-27 17:34:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -378,7 +378,7 @@ with st.sidebar.header('0. Select CSV or Model'):
             pred_ratio, pred_value = build_model(df)
             ### 이거이거이거이거이거
 
-            euclide_df = pd.DataFrame(columns=['공고번호', '낙찰하한율', '연면적', '대지면적', '기초금액', '예가율'])
+
 
             euclide_df = euclide_df.append({'공고번호': 9999, '낙찰하한율': ratio_list[ratio_value], '연면적': land_area, '대지면적': build_area, '기초금액': cost,'예가율': float(pred_ratio)}, ignore_index=True)
 
@@ -475,11 +475,11 @@ st.subheader('3. 유사공고 분석 ')
 st.write('유사공고 기업 분석')
 # euclide_df = pd.DataFrame(columns=['공고번호', '낙찰하한율', '연면적', '대지면적', '기초금액', '예가율'])
 
-if st.button("유사도분석"):
-    st.table(euclide_df)
+# if st.button("유사도분석"):
+st.table(euclide_df.tail(10))
 #     euclide_df = pd.DataFrame(columns=['공고번호', '낙찰하한율', '연면적', '대지면적', '기초금액', '예가율'])
 #
-#     concat_df = pd.read_csv('./euclidean.csv').astype(float)[['공고번호','낙찰하한율','연면적','대지면적','기초금액','예가율']]
+
 #     # concat_df=concat_df.append({'공고번호':9999,'낙찰하한율':float(ratio_list[ratio_value]),'연면적':float(land_area) ,'대지면적': float(build_area),'기초금액' : float(cost), '예가율' :float(pred_ratio)})
 #
 #     # euclidean_val = pd.DataFrame(squareform(pdist(concat_df.iloc[:, 1:])), columns=concat_df['공고번호'].unique(),index=concat_df['공고번호'].unique())
